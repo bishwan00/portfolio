12a18e3b8d3b27357730a2ed883624163c48dcdd
bishwan00 <bishwan.00606094@gmail.com>
2023-03-25 18:37:16 +0300

    ss

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,111 @@
-import React from 'react';
+import React from "react";
 
+import Image from "../assets/blob (1).svg";
+
+import { FaGithub, FaInstagram, FaFacebook } from "react-icons/fa";
+
+import { TypeAnimation } from "react-type-animation";
+
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
+import { fadeIn } from "../variants";
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center mb-60"
+      id="home"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
+            >
+              BISHWAN <span>SHERKO</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]"
+            >
+              <span className="text-white mr-4">I am a</span>
+              <TypeAnimation
+                sequence={["Developer", 2000, "Designer", 2000]}
+                speed={30}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
+              Welcome to my portfolio website! I am a full-stack developer with
+              a passion for creating innovative solutions to real-world
+              problems. With experience in both front-end and back-end
+              development, I have a diverse skill set that allows me to bring
+              unique perspectives to any project.
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
+              <Link to="contact" activeClass="active" smooth={true} spy={true}>
+                {" "}
+                <button className="btn btn-lg" to="contact">
+                  Contact me
+                </button>
+              </Link>
+              <a href="#" className="text-gradient btn-link">
+                Mt Portfolio
+              </a>
+            </motion.div>
+            <motion.div
+              variants={fadeIn("up", 0.8)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0 "
+            >
+              <a href="https://www.facebook.com/bishwan.sherko/">
+                {" "}
+                <FaFacebook />
+              </a>
+              <a href="https://github.com/bishwan00">
+                {" "}
+                <FaGithub />
+              </a>
+              <a href="https://www.instagram.com/bishwan_sherko/">
+                {" "}
+                <FaInstagram />
+              </a>
+            </motion.div>
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] "
+          >
+            <img src={Image} alt="" />
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import Logo from "../assets/logo.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className=" py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <div className="">
+            <span className="text-gradient">BISHWAN</span>
+            <div className="">
+              <span className="text-3xl text-white">SHERKO</span>
+            </div>
+          </div>
+
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
